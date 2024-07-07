ffb4012f3a2b718e70b87964ba100309be297b02
murytarlah <muritalajamiu2000@gmail.com>
2024-07-07 19:42:16 +0100

    define mock data for todos

--- index.js
+++ index.js
@@ -6,3 +6,27 @@ let todos = document.getElementById("list");
 
 // form 
 let form = document.getElementById("todo-form");
+
+// LS todos
+const todosLS = [
+    {
+        text: "Lorem Ipsum",
+        completed : false
+    },
+
+    {
+        text: "asdf jkl",
+        completed : true
+    },
+
+    {
+        text: "qwerty",
+        completed : false
+    },
+
+    {
+        text: "awdrgy",
+        completed : true
+    },
+
+]

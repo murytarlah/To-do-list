b2d9fb43c55130af3fa8a52cf5695d47da9010f0
murytarlah <muritalajamiu2000@gmail.com>
2024-07-07 19:41:37 +0100

    created reference to needed elements

---
+++ index.js
@@ -0,0 +1,8 @@
+// input element
+let input = document.getElementById("input-box");
+
+// todos
+let todos = document.getElementById("list");
+
+// form 
+let form = document.getElementById("todo-form");

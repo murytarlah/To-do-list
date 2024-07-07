b55c2c216b1918680656743bd397f55efa2c9bb2
murytarlah <muritalajamiu2000@gmail.com>
2024-07-07 19:43:37 +0100

    added addTodo function and the createTodoElement function

--- index.js
+++ index.js
@@ -30,3 +30,50 @@ const todosLS = [
     },
 
 ]
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addTodo(input)
+})
+
+
+function createTodoElement(text){
+    // creating the element
+    let li = document.createElement('li');
+
+    // add the text
+    li.textContent = text;
+
+    return li;
+}
+
+
+function addTodo(todo) {
+    let todoText = input.value
+
+    if(todo) {
+        todoText = todo.text
+    }
+
+    if(todoText) {
+        const todoEl = createTodoElement(todoText)
+        if(todo && todo.completed) {
+            todoEl.classList.add('completed')
+        }
+
+        todoEl.addEventListener('click', () => {
+            todoEl.classList.toggle('completed')
+        }) 
+
+        todoEl.addEventListener('contextmenu', (e) => {
+            e.preventDefault()
+
+            todos.removeChild(todoEl)
+        }) 
+
+        todos.appendChild(todoEl)
+
+        input.value = ''
+    }
+}
